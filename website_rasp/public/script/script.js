018ed8f5efa38556daee66bb604d198dc011add7
cyclerunner1 <meertrompet@gmail.com>
2020-04-04 23:16:01 +0200

    grid maken

--- website_rasp/public/script/script.js
+++ website_rasp/public/script/script.js
@@ -1,1 +1,21 @@
-console.log("test van script.js");
+const container = document.querySelector(".container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = `grid-item ${c + 1}`;
+  };
+};
+
+makeRows(5, 4);
+
+const griditem = document.querySelector(".grid-item");
+
+griditem.addEventListener('click', () => {
+    event.preventDefault();
+    console.log(this);
+});
